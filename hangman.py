0d9c926d2c1929901a18e05d26a38435a007ce24
okocsis90 <o.kocsis90@gmail.com>
2016-11-30 12:48:42 +0100

    Menu system

--- hangman.py
+++ hangman.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 import os
 import data_manager
 import random
+import time
 
 
 # This function gets the input from the user. It checks if the guess is
@@ -129,37 +130,61 @@ def check_if_won(word_to_display, total_lives, type_to_guess, to_guess, name, sc
     if total_lives == 0:
         os.system('clear')
         print("Too bad, GAME OVER! The solution was: %s" % to_guess)
-        return True
+        return score, dict_all_names, True
     # Check if we have any empty letters to guess:
     for word in word_to_display:
         if "_ " in word:
-            return False
+            return score, dict_all_names, False
     # If not, then player won
+    score, dict_all_names = score_gets_higher(name, score, current_mode, dict_all_names)
     print_table(word_to_display, total_lives, type_to_guess, name, score)
-    score_gets_higher(name, score, current_mode, dict_all_names)
     print("Congrats pal, you won!")
-    return True
+    return score, dict_all_names, True
 
 
 # asks player to enter a name or choose an existing one from the file
-def enter_name():
+def make_dict_from_all_names():
     all_names = data_manager.get_sentence_from_file("export.csv")
     dict_all_names = {}
     for i in all_names:
         dict_all_names.update({i.split(";")[0]: i.split(";")[1]})
+    return dict_all_names
+
+
+# choose from an existing name
+def choose_existing_name(dict_all_names):
+    os.system("clear")
     names = []
     for i in dict_all_names.keys():
         names.append(i)
     print()
     for i, name in enumerate(names, 1):
         print(i, ": ", name, "; score: ", dict_all_names[name])
-    new_name = data_manager.get_input("Please enter your name, or choose from the existing names: ")
-    for n in names:
-        if n == new_name:
-            score = dict_all_names[n]
-            return dict_all_names, n, score
+    while True:
+        option = data_manager.get_input("choose from the existing names: (or type 'back' to Main Menu): ")
+        if option.lower() == "back":
+            return dict_all_names, names[0], dict_all_names[names[0]], False
+        if option not in names:
+            print("Oops, no such name. Please try again!")
+            continue
+        for n in names:
+            if n == option:
+                score = dict_all_names[n]
+                return dict_all_names, n, score, True
+            else:
+                continue
+
+
+# set a new player
+def set_new_name(dict_all_names):
+    os.system("clear")
+    new_name = data_manager.get_input("Please enter a new name: (or type 'back' to Main Menu): ")
+    score = None
+    if new_name.lower() == "back":
+        return dict_all_names, new_name, score, False
+    dict_all_names.update({"new_name": "0"})
     score = 0
-    return dict_all_names, new_name, score
+    return dict_all_names, new_name, score, True
 
 
 # saves score of the player to our csv file.
@@ -170,32 +195,83 @@ def score_gets_higher(name, score, current_mode, dict_all_names):
         dict_all_names[name] = int(score) + 2
     elif current_mode == "h":
         dict_all_names[name] = int(score) + 3
+    score = str(dict_all_names[name])
     list_of_names = []
     for k, v in dict_all_names.items():
         list_of_names.append([k, str(v)])
     data_manager.save_data_to_file("export.csv", list_of_names)
+    return score, dict_all_names
 
 
-# We initialize the game, then in the main loop:
-# - print the table (word to be guessed and lives)
-# - get the input from the user and modify our board
-# - check if the player won / lose
-# - if so, check if he wants to play again
-def main():
-    dict_all_names, name, score = enter_name()
+def game_engine(dict_names, name, score, total_lives, current_mode):
     type_to_guess, to_guess, word_to_display = init_game()
-    total_lives, current_mode = init_lives()
     while True:
         print_table(word_to_display, total_lives, type_to_guess, name, score)
         word_to_display, total_lives = change_word_to_display(
             player_guess(word_to_display), word_to_display, to_guess, total_lives)
-        if check_if_won(word_to_display, total_lives, type_to_guess, to_guess, name, score, current_mode, dict_all_names):
+        score, dict_names, is_it_true = check_if_won(word_to_display, total_lives, type_to_guess,
+                                                     to_guess, name, score, current_mode, dict_names)
+        if is_it_true:
             if wanna_play_again():
                 type_to_guess, to_guess, word_to_display = init_game()
-                total_lives, current_mode = init_lives()
-                dict_all_names, name, score = enter_name()
             else:
                 break
+    return False
+
+
+def handle_main_menu():
+    os.system("clear")
+    print("Main Menu \nPlease choose an option: ")
+    print()
+    list_to_print = ["New game", "Choose existing player", "Exit"]
+    for i, op in enumerate(list_to_print, 1):
+        print(i, ": %s" % op)
+
+
+def choose():
+    option = input("Please enter a number: ")
+    if option == "1":
+        dict_names, name, score, is_it_true = set_new_name(make_dict_from_all_names())
+        total_lives = None
+        current_mode = None
+        if is_it_true is False:
+            return dict_names, name, score, total_lives, current_mode, False
+        total_lives, current_mode = init_lives()
+    elif option == "2":
+        dict_names, name, score, is_it_true = choose_existing_name(make_dict_from_all_names())
+        total_lives = None
+        current_mode = None
+        if is_it_true is False:
+            return dict_names, name, score, total_lives, current_mode, False
+        total_lives, current_mode = init_lives()
+    elif option == "3":
+        os.system("clear")
+        print("Bye!")
+        quit()
+    else:
+        raise KeyError
+    return dict_names, name, score, total_lives, current_mode, True
+
+
+def main():
+    while True:
+        handle_main_menu()
+        try:
+            dict_names, name, score, total_lives, current_mode, is_it_true = choose()
+            if is_it_true is False:
+                continue
+            os.system("clear")
+            if game_engine(dict_names, name, score, total_lives, current_mode) is False:
+                continue
+        except KeyError:
+            os.system("clear")
+            print("There is no such option!")
+            time.sleep(1)
+            continue
+        except (KeyboardInterrupt, EOFError):
+            os.system('clear')
+            print("bye")
+            quit()
 
 
 if __name__ == '__main__':
